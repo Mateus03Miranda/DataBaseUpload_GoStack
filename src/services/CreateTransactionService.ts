61dcb8498274a3a88d57d161b5122549a2c694ef
Mateus Miranda <mateusm.miranda80@gmail.com>
2020-12-30 14:33:42 -0300

    Concluido 'criar transaction' e 'listar as transactions'

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,30 @@
 // import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: RequestDTO): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+    });
+    await transactionRepository.save(transaction);
+    return transaction;
   }
 }
 
